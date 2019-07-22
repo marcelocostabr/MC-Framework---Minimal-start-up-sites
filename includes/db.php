5b052a01cecb80376b7326ac062f577da743e1c5
rda9000 <rda9000@users.noreply.github.com>
2019-07-22 14:49:09 -0300

    refatoração

--- includes/db.php
+++ includes/db.php
@@ -7,13 +7,6 @@ if ($_SERVER['SERVER_NAME']=="localhost") {
 	$pass 	= 'password';
 	$base 	= 'database_name';
 
-	$pdo = new PDO("mysql:host=$host;dbname=$base",$user,$pass);
-	$pdo->query("SET NAMES 'utf8'");
-	$pdo->query('SET character_set_connection=utf8');
-	$pdo->query('SET character_set_client=utf8');
-	$pdo->query('SET character_set_results=utf8');
-
-
 } else {
 
 	$host 	= 'mysql.remotepath.com.br';
@@ -21,12 +14,12 @@ if ($_SERVER['SERVER_NAME']=="localhost") {
 	$pass 	= 'password';
 	$base	= 'database_name';
 
-	$pdo = new PDO("mysql:host=$host;dbname=$base",$user,$pass);
-	$pdo->query("SET NAMES 'utf8'");
-	$pdo->query('SET character_set_connection=utf8');
-	$pdo->query('SET character_set_client=utf8');
-	$pdo->query('SET character_set_results=utf8');
-
 }
+
+$pdo = new PDO("mysql:host=$host;dbname=$base",$user,$pass);
+$pdo->query("SET NAMES 'utf8'");
+$pdo->query('SET character_set_connection=utf8');
+$pdo->query('SET character_set_client=utf8');
+$pdo->query('SET character_set_results=utf8');
 ?> 
 
